cd2ce56eb17354c9873cc067bc1a531824727761
ozergklp <ozergokalpsezer@gmail.com>
2023-11-01 14:07:26 +0300

    navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import ICON from './sloth.png'
+import Search from './Search'
+import { BiCartAlt } from "react-icons/bi"
 export default function Navbar() {
     return (
         <header>
             <nav>
-                <ul className='flex font-montserrat p-2 pt-2 font-semibold text-sm'>
+                <ul className='flex font-montserrat p-2 px-6 font-semibold text-sm '>
                     <li className='p-2 m-2 flex'>
                     <   img src={ICON} alt="sloth icon" width={50} className='mr-2'/>
-                        <p className='mt-2 font-bold'>SLOTH</p>
+                        <p className='mt-2 font-bold hover:underline cursor-pointer'>SLOTH</p>
                     </li>
-                    <li className='p-2 m-2 mt-4 text-slate-500'>All</li>
-                    <li className='p-2 m-2 mt-4 text-slate-500'>Shirts</li>
-                    <li className='flex-1 p-2 m-2 mt-4 text-slate-500'>Drinkware</li>
-                    <li className='p-2 m-2 mt-4 text-slate-500'>Search</li>
-                    <li className='p-2 m-2 mt-4 text-slate-500'>Cart</li>
+                    <li className='p-2 m-2 mt-4 text-slate-500 hover:underline cursor-pointer'>
+                        All</li>
+                    <li className='p-2 m-2 mt-4 text-slate-500 hover:underline cursor-pointer'>
+                        Shirts</li>
+                    <li className='flex-1 p-2 m-2 mt-4 text-slate-500 hover:underline cursor-pointer'>
+                        Drinkware</li>
+                    <Search />
+                    <li className='p-2 m-2 mt-3 text-2xl hover:text-blue-800 hover:underline cursor-pointer'>
+                        <BiCartAlt />
+                    </li>
                 </ul>
             </nav>
         </header>
