580d4b403f19a3f052029f229b62fcfe4e1a4b6a
ozergklp <ozergokalpsezer@gmail.com>
2023-11-06 22:43:07 +0300

    fixed some details

--- client/src/components/CartSideBar/CartItem.tsx
+++ client/src/components/CartSideBar/CartItem.tsx
@@ -1,34 +1,34 @@
-import { useDispatch } from "react-redux";
-import { formatPrice, getCartItemsFromLocalStorage } from "../../functions"
-import { FiTrash2, FiPlus } from "react-icons/fi";
+import { FiTrash2, FiPlus, FiMinus } from "react-icons/fi";
 import { ICartItem } from "../../types/type";
 
 export default function CartItem({item} : {item: ICartItem}) {
     const imageUrl = `http://localhost:4000/api/items/img/${item?.imagePath}`;
     return (
         <li  className=" text-lg bg-white flex mb-2">
-            <img src={imageUrl}className='w-[120px] h-[180px] object-cover'alt={'item'}/>
+            <img src={imageUrl} className='w-[70px] h-[70px]  object-scale-down rounded-xl'alt={'item'}/>
 
-            <section className="text-base w-[250px] mt-10">
+            <section className="text-base w-[250px]  flex flex-col">
 
                 <article className="flex justify-between ">
                 <p className="inline">{item.name}</p>
-                <p className='font-semibold inline-flex text-lg '>{`$${item.price.toFixed(2)} USD`}</p>
+                <p className='font-semibold inline-flex  '>{`$${item.price.toFixed(2)} USD`}</p>
                 
 
                 </article>
 
-                <br className="w-1"/>
-
                 <section className="flex justify-between ">
                     <p className="inline">Size {item.size?.label} {item.category}</p>
 
-                    <aside className="border-2 rounded-lg  w-[90px] mb-5 h-[40px] inline-flex text-lg ">
-                        <button className="mx-2  text-lg text-slate-700 hover:text-blue-700"
+                    <aside className="border-2 rounded-lg  w-[90px] mb-5 h-[40px] inline-flex ">
+                        <button className="mx-2   text-slate-700 hover:text-blue-700"
                                 >
-                                <FiTrash2 />
+                                {item.count > 1 ? (
+                                    <FiMinus />
+                                ) : (
+                                    <FiTrash2 />
+                                )}
                         </button>
-                        <h4 className="mx-2 mt-1">{1}</h4>
+                        <h4 className="mx-2 mt-1 text-lg">{1}</h4>
                         <button className="mx-2 text-slate-700  hover:text-blue-700">
                             <FiPlus /></button>
                     </aside>
